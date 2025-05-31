a7176e91c36eb250dd1846ddc1fb06944f76bf41
94prathampatil <07prathampatil06@gmail.com>
2025-05-31 20:29:00 +0530

    changes

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -69,7 +69,7 @@ export const purchaseCourse = async (req, res) => {
         }]
 
         const session = await stripeInstance.checkout.sessions.create({
-            success_url: `${origin}/loadin/my-enrollments`,
+            success_url: `${origin}loading/my-enrollments`,
             cancel_url: `${origin}/`,
             line_items: line_items,
             billing_address_collection: 'required',
